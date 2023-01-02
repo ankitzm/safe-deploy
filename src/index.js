7e478724db9ffaca5339495b54e57d1090f57203
ankitzm <ankit.singh.zm@gmail.com>
2023-01-02 23:41:10 +0530

    got file data in browser's console :fire:

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-function safeDeploy() {
-    console.log("efn");
-}
+import express from "express"
+import { readFile } from "fs"
 
-module.exports = safeDeploy
+const app = express()
+const port = 3000
+
+const fileName = process.argv[2]
+
+app.use(express.static("./src/public"))
+
+app.get("/file", (req, res) => {
+	console.log(fileName, req)
+	readFile(fileName, (err, data) => {
+		if (err) {
+			console.error(err)
+			res.status(500).send(`Error reading file: ${err}`)
+			return
+		}
+
+		res.send(data.toString())
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`)
+})

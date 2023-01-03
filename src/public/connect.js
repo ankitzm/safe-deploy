69f325f3c8d40e76e6b32dead32fa47909867eab
ankitzm <ankit.singh.zm@gmail.com>
2023-01-03 17:53:24 +0530

    added connector for metamsk

---
+++ src/public/connect.js
@@ -0,0 +1,39 @@
+if (typeof ethereum !== "undefined") {
+	// MetaMask is installed and enabled
+	console.log("eth object is available")
+} else {
+	// MetaMask is not installed or enabled
+	console.log("not enabled")
+}
+
+const ethereumButton = document.querySelector(".enableEthereumButton")
+const showAccount = document.querySelector(".showAccount")
+
+ethereumButton.addEventListener("click", () => {
+	getAccount()
+})
+
+async function getAccount() {
+	const accounts = await ethereum.request({
+		method: "eth_requestAccounts",
+	})
+	const account = accounts[0]
+	showAccount.innerHTML = account
+}
+
+
+
+// get binary
+
+const getBinaryBtn = document.querySelector(".getBinaryButton")
+
+let binaryData
+fetch("/file")
+	.then(response => response.text())
+	.then(data => {
+		binaryData = data
+	})
+
+getBinaryBtn.addEventListener("click", () => {
+	binaryData ? console.log(binaryData) : console.log("no data")
+})
